c43f1e1f2dd606df47c886cfd06dbecb2adaeb79
Wellington <36525792+nyyakko@users.noreply.github.com>
2026-03-22 13:18:26 -0300

    Initial commit

---
+++ _meta/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Gerando PDF"
+
+if docker \
+    run --rm -v ./:/app -v ./build:/app/build \
+        pandoc $(find . -path ./_meta -prune , -name "*.md" | sort) ./_meta/_referencias.md \
+        --bibliography ./src/bibliografia.bib \
+        -o build/output.pdf; then
+    echo "O PDF foi gerado com sucesso"
+else
+    echo "Falhas ao gerar o PDF"
+fi
+
