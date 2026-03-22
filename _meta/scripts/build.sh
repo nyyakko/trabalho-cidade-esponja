288ea37455270c13b620b38b6d18c767a393cde8
nyyakko <nyakonyns@gmail.com>
2026-03-22 18:06:26 -0300

    atualizar build script

--- _meta/scripts/build.sh
+++ _meta/scripts/build.sh
@@ -15,7 +15,7 @@ echo "Gerando PDF"
 
 if docker \
     run --rm -v ./:/app -v ./build:/app/build \
-        pandoc $(find . -path ./node_modules -prune , -path ./_meta -prune , -name "*.md" | sort) ./_meta/_referencias.md \
+        pandoc $(find . -path ./node_modules -prune , -path ./_meta -prune , -name "*.md" ! -name "README.md" | sort) ./_meta/_referencias.md \
         --bibliography ./src/bibliografia.bib \
         -o build/output.pdf; then
     echo "O PDF foi gerado com sucesso"
