e71e61f43a986167b16aed4da00a37e28934d493
nyyakko <nyakonyns@gmail.com>
2026-03-22 17:54:57 -0300

    atualizar build script

--- _meta/scripts/build.sh
+++ _meta/scripts/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if ! command -v docker -v > /dev/null; then
+    echo "Docker não está disponível em seu sistema, siga as instruções e instale: https://docs.docker.com/get-started/get-docker/"
+    exit 1
+fi
+
+if ! docker image inspect pandoc 1> /dev/null 2>&1; then
+    echo "Docker image não encontrada, realizando build neste instante"
+    docker build -t pandoc .
+    echo "Feito!"
+fi
+
 echo "Gerando PDF"
 
 if docker \
@@ -11,4 +22,3 @@ if docker \
 else
     echo "Falhas ao gerar o PDF"
 fi
-
